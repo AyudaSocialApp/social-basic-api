b0d1b356a78542e7431c77f844aa726003964fb0
Stivenson Rincón <stivenson.rpm@gmail.com>
2016-07-23 02:11:40 -0500

    New Field accepted in helps

---
+++ database/migrations/2016_07_23_070625_add_accepted_to_helps.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAcceptedToHelps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('helps', function (Blueprint $table) {
+            $table->boolean('accepted')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('helps', function (Blueprint $table) {
+            $table->dropColumn('accepted');
+        });
+    }
+}
